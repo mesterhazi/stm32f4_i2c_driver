9ff11a099adc2adddff62dfa1b7cd8c898dc1981
adam <adam.mesterhazi@gmail.com>
2019-10-05 19:56:55 +0200

    Initial commit, with empty files

---
+++ D_i2c.h
@@ -0,0 +1,27 @@
+/*
+ * D_i2c.h
+ *
+ *  Created on: 2019. okt. 5.
+ *      Author: adam
+ */
+
+#ifndef I2C_DRIVER_D_I2C_H_
+#define I2C_DRIVER_D_I2C_H_
+
+/* Typedefs */
+typedef struct {
+	// AddressMode
+	// OwnAddress1
+	// OwnAddress2
+	// DualAddressMode
+	// Speed
+	// NoStretchMode
+	// GeneralCallMode
+	// DmaEnable
+}D_I2C_InitTypeDef;
+
+/* Macros  */
+
+/* Function prototypes */
+
+#endif /* I2C_DRIVER_D_I2C_H_ */
